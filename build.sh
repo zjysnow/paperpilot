05c87cc5647dc200e93d9d80e06ffc5f23879476
AlbertZou <zjysnow@outlook.com>
2026-03-30 14:20:46 +0800

    init

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+PLUGIN_NAME="paperpilot"
+VERSION=$(python3 -c "import json; print(json.load(open('manifest.json'))['version'])")
+XPI_FILE="${PLUGIN_NAME}-${VERSION}.xpi"
+
+rm -f "$XPI_FILE"
+
+zip -r "$XPI_FILE" \
+  manifest.json \
+  bootstrap.js \
+  main.js \
+  prefs.js \
+  style.css \
+  locale/ \
+  -x "*.DS_Store" "build.sh" "*.xpi" "README.md"
+
+echo "Built: $XPI_FILE"
